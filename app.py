e2f3a430c022153e33e92600515f83a0325b870f
Mihály Orsós <ormraat.pte@gmail.com>
2024-11-26 10:46:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import plotly.graph_objects as go
+from goldhand import *
+
+
+tw = Tw()
+ticker= 'ETH-USD'
+t = GoldHand(ticker)
+p = t.plotly_last_year(tw.get_plotly_title(ticker))
+
+
 
 # Create Plotly plot
-fig = go.Figure(data=[go.Bar(x=['A', 'B', 'C'], y=[10, 20, 30])])
-st.plotly_chart(fig)
+#fig = go.Figure(data=[go.Bar(x=['A', 'B', 'C'], y=[10, 20, 30])])
+st.plotly_chart(p)
